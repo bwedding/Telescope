11d9f8fb2a99eeb683ad6a5db84fbb75b0e58fc6
xamfoo <xamfoo@all.repo>
2015-04-05 07:31:38 +0800

    Update package description

--- packages/telescope-bootstrap-wysiwyg/package.js
+++ packages/telescope-bootstrap-wysiwyg/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'telescope-bootstrap-wysiwyg',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Change the post editor to bootstrap-wysiwyg',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -13,11 +13,15 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.5');
   api.use(['less', 'templating'], 'client');
+
   api.use(['telescope-base', 'aldeed:autoform', 'djedi:sanitize-html'], 'client');
+
   api.addFiles('bootstrap/bootstrap.min.js', 'client', {bare: true});
   api.addFiles('bootstrap/bootstrap.less', 'client');
+
   api.addFiles('wysiwyg/bootstrap-wysiwyg.js', 'client', {bare: true});
   api.addFiles('wysiwyg/style.less', 'client');
+
   api.addFiles([
     'client/views/forms/bootstrap-wysiwyg.html',
     'client/views/forms/bootstrap-wysiwyg.js',
@@ -26,11 +30,11 @@ Package.onUse(function(api) {
     'client/views/posts/post_edit.html',
     'client/views/posts/post_submit.html',
   ], 'client');
+
   api.addFiles('index.js', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('telescope-bootstrap-wysiwyg');
-  api.addFiles('telescope-bootstrap-wysiwyg-tests.js');
 });
