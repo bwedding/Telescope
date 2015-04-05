1977cbef3f72923d60faa862eda9d081d363828d
xamfoo <xamfoo@all.repo>
2015-04-05 15:00:51 +0800

    Add editor button for image

--- packages/telescope-bootstrap-wysiwyg/client/views/forms/bootstrap-wysiwyg.js
+++ packages/telescope-bootstrap-wysiwyg/client/views/forms/bootstrap-wysiwyg.js
@@ -9,6 +9,12 @@ Template["afBootstrap-wysiwyg"].onRendered(function () {
   var inst = this;
   inst.$('#bootstrap-wysiwyg').wysiwyg();
   inst.$('#bootstrap-wysiwyg').html(inst.data.value);
+  inst.$('[data-role=magic-overlay]').each(function () { 
+    var overlay = $(this), target = $(overlay.data('target')); 
+    overlay.css('opacity', 0).css('position', 'absolute')
+    .offset(target.offset()).width(target.outerWidth())
+    .height(target.outerHeight());
+  });
 });
 
 Template["afBootstrap-wysiwyg"].events({
